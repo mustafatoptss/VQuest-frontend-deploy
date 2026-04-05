fbcd44003fff216cd6fe9c75539942c6cfc81e46
mustafatoptss <159811845+mustafatoptss@users.noreply.github.com>
2026-04-05 14:21:10 +0300

    login hatası

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://vquest-backend-api.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
